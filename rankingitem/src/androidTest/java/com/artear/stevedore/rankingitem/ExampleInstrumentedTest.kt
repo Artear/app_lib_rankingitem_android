da4927a9630eaa19d4867a9585e6daa605722a1f
sergiobanares <sergio.banares@gmail.com>
2019-07-23 17:44:31 -0300

    Change: Folders Fixed

--- rankingitem/src/androidTest/java/com/artear/stevedore/rankingitem/ExampleInstrumentedTest.kt
+++ rankingitem/src/androidTest/java/com/artear/stevedore/rankingitem/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.artear.rankingitem
+package com.artear.stevedore.rankingitem
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().context
-        assertEquals("com.artear.rankingitem", appContext.packageName)
+        assertEquals("com.artear.stevedore.rankingitem", appContext.packageName)
     }
 }
--- rankingitem/src/test/java/com/artear/stevedore/rankingitem/ExampleUnitTest.kt
+++ rankingitem/src/test/java/com/artear/stevedore/rankingitem/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.artear.rankingitem
+package com.artear.stevedore.rankingitem
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
